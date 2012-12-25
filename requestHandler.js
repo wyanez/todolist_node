2038be6799b3426be187d3be5dc88520b74b078c
William Yanez <wyanez@gmail.com>
2012-12-24 19:47:56 -0430

    Refactoring del RequestHandler, implementacion del metodo redirect_to

--- requestHandler.js
+++ requestHandler.js
@@ -5,29 +5,30 @@ var querystring = require('querystring');
 
 var appURL = 'http://localhost:8888';
 
-function add(request,response){
-	console.log('[requestHandler] Action add was called.');
-
-	//Redirecciono a list
-	response.writeHead(301, {'Location':appURL + '/list', 'Expires': (new Date).toGMTString()});
-	response.end();
+function list(request,response){
+    console.log('[requestHandler] Action list was called.');
+    
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('<h1>ToDo List</h1>');
+    response.end();
 }
 
-function list(request,response){
-	console.log('[requestHandler] Action list was called.');
-	
-	response.writeHead(200, {'Content-Type': 'text/html'});
-	response.write('<h1>ToDo List</h1>');
-	response.end();
+function add(request,response){
+	console.log('[requestHandler] Action add was called.');
+	redirect_to('/list',response);
 }
 
 function remove(request,response){
 	console.log('[requestHandler] Action remove was called.');
-    
-    //Redirecciono a list
-    response.writeHead(301, {'Location':appURL + '/list', 'Expires': (new Date).toGMTString()});
-    response.end();
+    redirect_to('/list',response);
 }    
+
+function redirect_to(action,response){
+    //Redirecciono a list
+    console.log('[requestHandler] Redirect to '+action);
+    response.writeHead(301, {'Location':appURL + action, 'Expires': (new Date).toGMTString()});
+    response.end();
+}
 
 exports.add = add;
 exports.list = list;
